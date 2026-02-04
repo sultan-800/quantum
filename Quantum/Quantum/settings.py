7b146e13c9f0d2c92c5cec7ed0eee4f410b979fc
sultan-800 <deronnn1@gmail.com>
2026-02-04 22:04:11 +0300

    Базовая структура настроена

--- Quantum/Quantum/settings.py
+++ Quantum/Quantum/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'main',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -115,3 +116,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+]
